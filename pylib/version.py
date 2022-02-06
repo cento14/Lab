90812835753ecfa319512495c3fce9e226716277
Andrea Giuliani <cento14@gmail.com>
2022-02-06 17:24:10 +0100

    modified:   pylib/version.py 	new file:   pylib/version2.py

--- pylib/version.py
+++ pylib/version.py
@@ -2,9 +2,9 @@
 
 import gammapy, astropy, numpy, scipy, matplotlib
 
-print 'Gammapy: ', gammapy.__version__
-print 'Astropy: ', astropy.__version__
-print 'Scipy: ',scipy.__version__
-print 'Numpy: ',numpy.__version__
-print 'Matplotlib: ',matplotlib.__version__
-print 'pippo'
+print('Gammapy: ', gammapy.__version__)
+print('Astropy: ', astropy.__version__)
+print('Scipy: ',scipy.__version__)
+print('Numpy: ',numpy.__version__)
+print('Matplotlib: ',matplotlib.__version__)
+print('pippo')
---
+++ pylib/version2.py
@@ -0,0 +1,10 @@
+
+
+import gammapy, astropy, numpy, scipy, matplotlib
+
+print 'Gammapy: ', gammapy.__version__
+print 'Astropy: ', astropy.__version__
+print 'Scipy: ',scipy.__version__
+print 'Numpy: ',numpy.__version__
+print 'Matplotlib: ',matplotlib.__version__
+print 'pippo'
